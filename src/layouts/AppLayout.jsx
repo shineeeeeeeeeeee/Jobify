38caa3a5231a3091a962c55dbd0d6326b4119aa1
Shine Suri <139244550+shineeeeeeeeeeee@users.noreply.github.com>
2025-11-23 14:37:25 +0530

    Update AppLayout.jsx

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -12,7 +12,7 @@ const AppLayout = () => {
           <Outlet />
         </main>
 
-        <div className='p-10 text-center bg-gray-800 mt-10'>Made by Shine</div>
+        <div className='p-10 text-center bg-gray-800 mt-10'>© 2025 Jobify | Designed & Developed by Shine</div>
 
     </div>
   )
